217c1354b1b3d169aa1c8aa7cabfe92aa73997ff
Veronica Bustabad <vbustabad@gmail.com>
2018-04-06 01:58:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,8 +29,8 @@ class Owner
 
   def buy_fish(name)
     new_fish = Fish.new(name)
-    self.pets.[:fishes] = new_fish
-    self.pets.[:fishes][0]
+    self.pets[:fishes] = new_fish
+    self.pets[:fishes][0]
   end
 
   def buy_cat(name)
