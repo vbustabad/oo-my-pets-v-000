af87bc59e590f48bfe8241f799a2ab52cde96985
Veronica Bustabad <vbustabad@gmail.com>
2018-04-06 00:14:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,59 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@owners = []
+  @@owner_count = 0
+
+  def initialize(species)
+    @species = "human"
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@owners << self
+    @@owner_count = @@owner_count + 1
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owner_count
+  end
+
+  def self.reset_all
+    @@owner_count = 0
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    self.pets.[:fishes] = new_fish
+    self.pets.[:fishes][0]
+  end
+
+  def buy_cat(name)
+
+  end
+
+  def buy_dog(name)
+
+  end
+
+  def walk_dogs
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
+
 end
